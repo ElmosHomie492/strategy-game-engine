72aaa94e8918733b69a476a37805e9a44c0a9387
Kaleb Walker <36672021+ElmosHomie492@users.noreply.github.com>
2022-04-20 12:27:49 -0500

    chore!: adding configuration defaults

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 func main() {
 	config := initEngine("configuration")
+	
+	var config internal.Configuration
+
+	setup.BuildConfiguration(
+		viper.New(),
+		&config,
+		*internal.ConfigurationDefaults,
+	)
 
 	config.Logger.Info().Msg(config.FirstValue)
 }
